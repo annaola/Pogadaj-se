22a5ccc4de05fe3a374d01cdecc4f17a106be862
iCarrrot <michal.mar3@gmail.com>
2017-12-21 09:54:16 +0100

    test version

--- server.js
+++ server.js
@@ -1,24 +1,67 @@
-var http = require('http');
-var express = require('express');
-var url = require('url')
-var utils = require('./utils.js');
+/*
+ * Manage Session in Node.js and ExpressJS
+ * Author : Shahid Shaikh
+ * Version : 0.0.1
+*/
+var express		=	require('express');
+var session		=	require('express-session');
+var bodyParser  	= 	require('body-parser');
+var app			=	express();
 
-var app = express();
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
-app.get('/', (req, res) => {
-    var model = { https: 0, http: 1 };
-    res.render('main', model);
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({extended: true}));
+
+var sess;
+
+app.get('/',function(req,res){
+	sess=req.session;
+	if(sess.email)
+	{
+		res.redirect('/admin');
+	}
+	else{
+	res.render('index.html');
+	}
 });
 
-
-app.use((req, res, next) => {
-    res.render('404.ejs', { url: req.url });
+app.post('/login',function(req,res){
+	sess=req.session;	
+	sess.email=req.body.email;
+	res.end('done');
 });
 
+app.get('/admin',function(req,res){
+	sess=req.session;
+	if(sess.email)	
+	{
+		res.write('<h1>Hello '+sess.email+'</h1><br>');
+		res.end('<a href='+'/logout'+'>Logout</a>');
+	}
+	else
+	{
+		res.write('<h1>Please login first.</h1>');
+		res.end('<a href='+'/'+'>Login</a>');
+	}
 
-// tu uruchamiamy serwer
-var server = http.createServer(app).listen( process.env.PORT || 3000 );
-// var server = https.createServer(options, app1).listen(3001);
-console.log('serwer started');
+});
+
+app.get('/logout',function(req,res){
+	
+	req.session.destroy(function(err){
+		if(err){
+			console.log(err);
+		}
+		else
+		{
+			res.redirect('/');
+		}
+	});
+
+});
+app.listen(3000,function(){
+	console.log("App Started on PORT 3000");
+});
